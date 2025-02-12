940f4700f27d177ca843ac8a66711ba26e7efdd3
lilimanzo <lmanzo1@uci.edu>
2025-02-12 11:11:15 -0800

    test395 remove test from 393

--- columnphysics/icepack_therm_shared.F90
+++ columnphysics/icepack_therm_shared.F90
@@ -147,7 +147,7 @@
 
       ! longwave radiative flux
       flwdabs =  emissivity * flw
-      flwoutn = -emissivity * stefan_boltzmann * TsfK**4 * 0.5 ! LM added .5
+      flwoutn = -emissivity * stefan_boltzmann * TsfK**4 !* 0.5 ! LM added .5
 
       ! downward latent and sensible heat fluxes
       fsensn = shcoef * (potT - TsfK)
