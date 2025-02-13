58f8f0079954c4163fe8da04d30f94b925766c86
lilimanzo <lmanzo1@uci.edu>
2025-02-13 14:11:54 -0800

    test404 try to fix TsfK data type ice_therm_shared

--- columnphysics/icepack_therm_shared.F90
+++ columnphysics/icepack_therm_shared.F90
@@ -130,7 +130,7 @@
 
       ! local variables
       real(kind=dbl_kind) :: &
-         TsfK        , & ! ice/snow surface temperature (K)
+         !TsfK        , & ! ice/snow surface temperature (K) - LM commented
          Qsfc        , & ! saturated surface specific humidity (kg/kg)
          qsat        , & ! the saturation humidity of air (kg/m^3)
          flwdabs     , & ! downward longwave absorbed heat flx (W/m^2)
