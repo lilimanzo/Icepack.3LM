34171b495e305a957769d63d9786f56264814e98
lilimanzo <lmanzo1@uci.edu>
2025-02-24 16:17:33 -0800

    test471 comment all TsfKr

--- columnphysics/icepack_flux.F90
+++ columnphysics/icepack_flux.F90
@@ -65,8 +65,8 @@
                                Uref,     Urefn,      &
                                Qref_iso, Qrefn_iso,  &
                                fiso_ocn, fiso_ocnn,  &
-                               fiso_evap, fiso_evapn,&
-                               TsfKr) ! LM added
+                               fiso_evap, fiso_evapn)!,&
+                               !TsfKr) ! LM added
 
       ! single category fluxes
       real (kind=dbl_kind), intent(in) :: &
@@ -123,7 +123,7 @@
           evaps   , & ! evaporation over snow           (kg/m2/s)
           evapi   , & ! evaporation over ice            (kg/m2/s)
           Tref    , & ! air tmp reference level         (K)
-          TsfKr   , & ! LM radiative temp               (K)
+          !TsfKr   , & ! LM radiative temp               (K)
           Qref    , & ! air sp hum reference level      (kg/kg)
           fresh   , & ! fresh water flux to ocean       (kg/m2/s)
           fsalt   , & ! salt flux to ocean              (kg/m2/s)
@@ -196,8 +196,8 @@
          evapi      = evapi    + evapin    * aicen
       if (present(Trefn) .and. present(Tref)) &
          Tref       = Tref     + Trefn     * aicen
-      if (present(TsfK) .and. present(TsfKr)) &    ! LM added
-         TsfKr      = TsfKr    + TsfK     * aicen ! LM added
+      !if (present(TsfK) .and. present(TsfKr)) &    ! LM added
+      !   TsfKr      = TsfKr    + TsfK     * aicen ! LM added
       if (present(Qrefn) .and. present(Qref)) &
          Qref       = Qref     + Qrefn     * aicen
 
